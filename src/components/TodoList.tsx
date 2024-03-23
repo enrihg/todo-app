699575a1db16520aa520bb1e415f243fd3524a0e
enrihg <99612570+enrihg@users.noreply.github.com>
2024-03-23 17:19:07 -0300

    todo item component added

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import TodoItem from "./TodoItem";
 
 const initialTodos = [
     { id: 1, text: 'comprar yerba', completed: false },
     { id: 2, text: 'comprar verdura', completed: false },
-    { id: 3, text: 'lavar la ropa', completed: false },
+    { id: 3, text: 'lavar la ropa', completed: true },
     { id: 4, text: 'terminar el todo proyect', completed: false },
 ]
 
@@ -12,19 +13,11 @@ function TodoList(): JSX.Element {
 
     return (
         <ul>
-            {todos.map(todo => (
-                <li key={todo.id} className=" flex justify-between border-b-solid-[1px] h-[52px] py-4 px-5 bg-white">
-                    <button className="w-5 h-5 rounded-full bg-black">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6" /></svg>
-                    </button>
-                    {todo.text}
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" /></svg>
-                    </button>
-                </li>
+            {todos.map((todo) => (
+                <TodoItem id={todo.id} text={todo.text} completed={todo.completed}/>
             ))}
         </ul>
     )
-}
+}   
 
 export default TodoList
