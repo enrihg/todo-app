e5dc9f656c8a64c9a857fb58ce039d51b55a349a
enrihg <99612570+enrihg@users.noreply.github.com>
2024-03-24 19:47:56 -0300

    add todo added

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,30 @@
+import { ChangeEvent, useState } from "react";
+
+function TodoForm({ addTodo }) {
+    const [text, setText] = useState("");
+
+    const handleSubmit = (evt:SubmitEvent) => {
+        evt.preventDefault();
+        addTodo(text);
+        setText("");
+    }
+
+    const handleChange = (evt:ChangeEvent) => {
+        setText(evt.target.value);
+    }
+
+
+
+    return (
+        <li className=" flex justify-between border-b-solid-[1px] h-[52px] py-4 px-5 bg-white mb-4">
+            <form onSubmit={handleSubmit}>
+                <button className="w-5 h-5 rounded-full bg-black">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" strokeWidth="2" d="M1 4.304L3.696 7l6-6" /></svg>
+                </button>
+                <input type="text" value={text} onChange={handleChange} placeholder="Create a new todo..." />
+            </form>
+        </li>
+    )
+}
+
+export default TodoForm
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import TodoItem from "./TodoItem";
+import TodoForm from "./TodoForm";
 
 const initialTodos = [
-    { id: 1, text: 'comprar yerba', completed: false },
+    { id: 1, text: 'comprar yerba', completed: true },
     { id: 2, text: 'comprar verdura', completed: false },
     { id: 3, text: 'lavar la ropa', completed: true },
     { id: 4, text: 'terminar el todo proyect', completed: false },
@@ -11,8 +12,15 @@ const initialTodos = [
 function TodoList(): JSX.Element {
     const [todos, setTodos] = useState(initialTodos);
 
+    const addTodo = (text) => {
+        setTodos(prevTodos => {
+        return [...prevTodos, { text:text, id: 8, completed:false}]
+        })
+    }
+
     return (
         <ul>
+            <TodoForm addTodo={addTodo}/>
             {todos.map((todo) => (
                 <TodoItem id={todo.id} text={todo.text} completed={todo.completed}/>
             ))}
