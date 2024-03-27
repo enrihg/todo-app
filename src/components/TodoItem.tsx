dc038d9c78780d12accfba32b896f7cbb5788a33
enrihg <99612570+enrihg@users.noreply.github.com>
2024-03-27 19:19:52 -0300

    toggle completed todos added

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type ComponentProps = {
     todo: {
         id: string,
@@ -8,6 +10,12 @@ type ComponentProps = {
 }
 
 function TodoItem({ todo, remove }: ComponentProps): JSX.Element {
+    const [completed, setCompleted] = useState(todo.completed)
+
+    const toggleCompleted = () => {
+        setCompleted(!completed);
+        console.log(completed);
+    }
 
     const removeTodo = () => {
         console.log(todo.id)
@@ -16,12 +24,11 @@ function TodoItem({ todo, remove }: ComponentProps): JSX.Element {
 
     return (
         <li key={todo.id} className=" flex justify-between border-b-solid-[1px] h-[52px] py-4 px-5 bg-white">
-            <button className="w-5 h-5 rounded-full bg-black">
+            <button onClick={toggleCompleted} className="w-5 h-5 rounded-full bg-black">
                 <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" strokeWidth="2" d="M1 4.304L3.696 7l6-6" /></svg>
             </button>
-            <input type="checkbox" name="" id="" />
             {todo.text}
-            <span>{todo.completed ? "completed" : "not completed"}</span>
+            {/* <span>{completed ? "completed" : "not completed"}</span> */}
             <button onClick={removeTodo}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" /></svg>
             </button>
