9dcd0f9e8b4a3046501abc6a5a0ef5131012d29d
enrihg <99612570+enrihg@users.noreply.github.com>
2024-04-07 13:49:19 -0300

    app component refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,54 @@
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import Main from './components/Main';
 import Header from './components/Header';
+import TodoForm from "./components/TodoForm";
 import TodoList from './components/TodoList'
 
+const getInitialData = () => {
+  const data = JSON.parse(localStorage.getItem("todos")!);
+  if (!data) return [];
+  return data;
+}
+
 function App(): JSX.Element {
+  const [todos, setTodos] = useState(getInitialData);
+  const [completed, setCompleted] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (text: string) => {
+    setTodos(prevTodos => {
+      return [...prevTodos, { text: text, id: uuidv4(), completed: completed }]
+    })
+  }
+
+  const removeTodo = (id: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((t) => t.id !== id);
+    })
+  }
+
+  const toggleCompleted = () => {
+    setCompleted(!completed);
+  }
+
+  const clearCompleted = () => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((t) => {
+        t.complete === true; console.log(todos.length)
+      })
+    })
+  }
+
   return (
     <Main>
-      <Header/>
-      <TodoList />
+      <Header />
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} clearCompleted={clearCompleted} removeTodo={removeTodo} />
     </Main>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,61 +1,17 @@
-import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
-import Header from "./Header";
 import TodoItem from "./TodoItem";
-import TodoForm from "./TodoForm";
 
-const getInitialData = () => {
-    const data = JSON.parse(localStorage.getItem("todos")!);
-    if (!data) return [];
-    return data;
-}
-
-function TodoList(): JSX.Element {
-    const [todos, setTodos] = useState(getInitialData);
-    const [completed, setCompleted] = useState(false)
-
-    useEffect(() => {
-        localStorage.setItem("todos", JSON.stringify(todos));
-    }, [todos]);
-
-    const addTodo = (text: string) => {
-        setTodos(prevTodos => {
-            return [...prevTodos, { text: text, id: uuidv4(), completed: completed }]
-        })
-    }
-
-    const removeTodo = (id: string) => {
-        setTodos((prevTodos) => {
-            return prevTodos.filter((t) => t.id !== id);
-        })
-    }
-
-    const toggleCompleted = () => {
-        setCompleted(!completed);
-    }
-
-    const clearCompleted = () => {
-        setTodos((prevTodos) => {
-            return prevTodos.filter((t) => {
-                t.complete === true; console.log(todos.length)
-            })
-        })
-    }
+function TodoList({ todos, clearCompleted, removeTodo }): JSX.Element {
 
     return (
-        <>
-            {/* <Header/> */}
-            <TodoForm addTodo={addTodo} />
-            <ul className="rounded-[5px] divide-y overflow-hidden shadow-shadow">
-                {todos.map((todo: { id: string; text: string; completed: boolean; }) => (
-                    <TodoItem key={todo.id} id={todo.id} text={todo.text} completed={todo.completed} remove={removeTodo} />
-                ))}
-                <li className="flex justify-between bg-white dark:bg-dark-800 text-light-400 text-xs pt-4 px-5 pb-5">
-                    <span>{todos.length} items left</span>
-                    <button onClick={clearCompleted}>Clear Completed</button>
-                </li>
-            </ul>
-        </>
+        <ul className="rounded-[5px] divide-y overflow-hidden shadow-shadow">
+            {todos.map((todo: { id: string; text: string; completed: boolean; }) => (
+                <TodoItem key={todo.id} id={todo.id} text={todo.text} completed={todo.completed} remove={removeTodo} />
+            ))}
+            <li className="flex justify-between bg-white dark:bg-dark-800 text-light-400 text-xs pt-4 px-5 pb-5">
+                <span>{todos.length} items left</span>
+                <button onClick={clearCompleted}>Clear Completed</button>
+            </li>
+        </ul>
     )
 }
 
