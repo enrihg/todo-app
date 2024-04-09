4f1e64c1f0fde8e3bb7d329ceb83f07d8849179e
enrihg <99612570+enrihg@users.noreply.github.com>
2024-04-09 12:02:28 -0300

    clear completed function added

--- src/App.tsx
+++ src/App.tsx
@@ -7,53 +7,51 @@ import TodoForm from "./components/TodoForm";
 import TodoList from './components/TodoList'
 
 const getInitialData = () => {
-  const data = JSON.parse(localStorage.getItem("todos")!);
-  if (!data) return [];
-  return data;
+    const data = JSON.parse(localStorage.getItem("todos")!);
+    if (!data) return [];
+    return data;
 }
 
 function App(): JSX.Element {
-  const [todos, setTodos] = useState(getInitialData);
+    const [todos, setTodos] = useState(getInitialData);
 
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
 
-  const addTodo = (text: string) => {
-    setTodos(prevTodos => {
-      return [...prevTodos, { text: text, id: uuidv4(), completed: false }]
-    })
-  }
+    const addTodo = (text: string) => {
+        setTodos(prevTodos => {
+            return [...prevTodos, { text: text, id: uuidv4(), completed: false }]
+        })
+    }
 
-  const removeTodo = (id: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((t) => t.id !== id);
-    })
-  }
+    const removeTodo = (id: string) => {
+        setTodos((prevTodos) => {
+            return prevTodos.filter((t) => t.id !== id);
+        })
+    }
 
-  const toggleCompleted = (id: string) => {
-    setTodos((item) =>
-      item.map((todo) => (
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      ))
-    )
-  }
+    const toggleCompleted = (id: string) => {
+        setTodos((item) =>
+            item.map((todo) => (
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            ))
+        )
+    }
 
-  const clearCompleted = () => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((t) => {
-        t.completed === true; console.log(todos.length)
-      })
-    })
-  }
+    const clearCompleted = () => {
+        todos.forEach(todo => {
+            todo.completed === false ? todo : removeTodo(todo.id)
+        });
+    }
 
-  return (
-    <Main>
-      <Header />
-      <TodoForm addTodo={addTodo} />
-      <TodoList todos={todos} toggleCompleted={toggleCompleted} clearCompleted={clearCompleted} removeTodo={removeTodo} />
-    </Main>
-  );
+    return (
+        <Main>
+            <Header />
+            <TodoForm addTodo={addTodo} />
+            <TodoList todos={todos} toggleCompleted={toggleCompleted} clearCompleted={clearCompleted} removeTodo={removeTodo} />
+        </Main>
+    );
 }
 
 export default App;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,13 +9,19 @@ type ComponentProps = {
 
 function TodoList({ todos, toggleCompleted, clearCompleted, removeTodo }: ComponentProps): JSX.Element {
 
+    let itemsCompleted = 0;
+
+   todos.forEach(todo => {
+        todo.completed ? itemsCompleted++ : itemsCompleted
+   });
+
     return (
         <ul className="rounded-[5px] divide-y overflow-hidden shadow-shadow">
             {todos.map((todo) => (
                 <TodoItem key={todo.id} id={todo.id} text={todo.text} completed={todo.completed} toggle={toggleCompleted} remove={removeTodo} />
             ))}
             <li className="flex justify-between bg-white dark:bg-dark-800 text-light-400 text-xs pt-4 px-5 pb-5">
-                <span>{todos.length} items left</span>
+                <span>{todos.length- itemsCompleted} items left</span>
                 <button onClick={clearCompleted}>Clear Completed</button>
             </li>
         </ul>
